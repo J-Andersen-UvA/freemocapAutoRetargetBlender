eab9bc36461d1f895e28dc81e908ebd316d0ecf8
J-Andersen-UvA <j.andersen@uva.nl>
2024-08-01 16:02:41 +0200

    added the avatar to the github page, also changed the scripts to use this.

--- blenderScripts/importGlassesGuy.py
+++ blenderScripts/importGlassesGuy.py
@@ -1,11 +1,14 @@
 import bpy
+import os
 
-# Define the path to your FBX file
-fbx_file_path = r"C:\Users\VICON\Desktop\gg_posedTo_freemocap.fbx"
+# Get the directory of the directory of the current script
+script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Define the relative path to your FBX file
+fbx_file_path = os.path.join(script_dir, "avatars/gg_posedTo_freemocap.fbx")
 
 # Import the FBX file with the 'automatic_bone_orientation' option enabled
 bpy.ops.import_scene.fbx(filepath=fbx_file_path, automatic_bone_orientation=True)
-
 
 # Apply all transforms to the imported objects
 # Switch to Object mode
